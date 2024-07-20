01c0980048662bc46703ba6405e731222f349567
Faustharus <chailloleau-damien@outlook.fr>
2024-07-20 19:16:09 +0200

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Damien Chailloleau on 20/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
